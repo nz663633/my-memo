27ea0321d61a6b42e04d8d181f023f4c7647c88f
nz663633 <hyunjisim3@gmail.com>
2026-03-17 16:24:16 +0900

    refactor: move mockData to App.jsx for global state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,21 @@ import Header from './components/Header';
 import Login from './components/Login';
 import Editor from './components/Editor';
 import List from './components/List';
+import { useState } from 'react';
 
 function App() {
+  const [mockData, setMockData] = useState([
+    {
+      id: 0,
+      title: '첫 메모',
+      content: '안녕하세요.'
+    },
+    {
+      id: 1,
+      title: '두 번째 메모',
+      content: '이것은 두 번째 메모입니다.'
+    }
+  ]);
   return (
     <>
       <div className='App'>
@@ -14,7 +27,7 @@ function App() {
           <Editor />
         </div>
         <div className='rightApp'>
-          <List />
+          <List mockData={mockData}/>
         </div>
       </div>
     </>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,27 +1,14 @@
 import { useState } from "react";
 import Item from "./Item.jsx";
 
-const List = () => {
-    const mockData = [
-        {
-            id: 0,
-            title: '첫 메모',
-            content: '안녕하세요.'
-        },
-        {
-            id: 1,
-            title: '두 번째 메모',
-            content: '이것은 두 번째 메모입니다.'
-        }
-    ];
-
+const List = ({ mockData }) => {
     return (
         <>
             {mockData.map((item) => (
                 <Item
-                key={item.id} 
-                title={item.title}
-                content={item.content}/>
+                    key={item.id}
+                    title={item.title}
+                    content={item.content} />
             ))}
         </>
     )
