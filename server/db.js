8e552f2d8e0509a447c3c91290bfdd49a360422b
nz663633 <hyunjisim3@gmail.com>
2026-08-31 14:37:02 +0900

    debug: check database connection

--- server/db.js
+++ server/db.js
@@ -8,6 +8,11 @@ const app = express(); // express 서버 객체 생성
 // .env 파일을 읽어서 process.env에 등록
 dotenv.config();
 
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_PORT:", process.env.DB_PORT);
+
 export const database = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
