d934be0f1311d81c2f4e16e773f12651bb750cf9
nz663633 <hyunjisim3@gmail.com>
2026-08-31 14:44:40 +0900

    fix: update database connection for Railway deployment

--- server/db.js
+++ server/db.js
@@ -8,17 +8,12 @@ const app = express(); // express 서버 객체 생성
 // .env 파일을 읽어서 process.env에 등록
 dotenv.config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PORT:", process.env.DB_PORT);
-
 export const database = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 
